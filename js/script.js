0cf0a60cc6d74b373f0406eedc661fbaac2a9da5
SampsonNg <sampsonngqianhao@hotmail.com>
2018-04-28 22:24:48 +0800

    rocket

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 console.log('Hiiiii friends')
-var car ={
+
+var docoolstuff = function(){
+	var thatclass = document.getElementById('cool').className;
+	if (thatclass == 'cool'){
+		document.getElementById('cool').className='cool red';
+	}else{
+		document.getElementById('cool').className='cool';
+	}
+};
+
+var car = {
 	make:'toyota',
 	type:'altis',
 	year: 2018,
@@ -9,16 +19,19 @@ var car ={
 	'seat1',
 	'seat2',
 	'seat3',
-	'seat4'
+	'seat4',
 	],
-	switchCar: function(ison){
+	switchcar: function(ison){
 		if (ison == true) {
-			this.isturnon = true
-			return ('Car is now turned on.')
+			this.isturnon = true;
+			console.log ('Car is now turned on.')
 		} else {
-			this.isturnon = false
-			return('Car is now turned off.')
+			this.isturnon = false;
+			console.log('Car is now turned off.')
 		}
 
+	},
+	fly:function(){
+		alert('fly')
 	}
 }
